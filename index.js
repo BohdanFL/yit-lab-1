22cc4f5494ce2ce23594f0272f96f80a01b15ec5
BohdanFL <robot228rb@gmail.com>
2025-03-15 15:56:02 +0200

    Init project

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello World!",
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
